0fb7fcc672d86d586802ef241bbe5015679afce8
DuanDuan Pan <enjoyjavapan@163.com>
2017-05-03 15:34:24 +0800

    complete Chapter 04

--- simple-weather/src/main/java/com/cssrc/study/maven/App.java
+++ simple-weather/src/main/java/com/cssrc/study/maven/App.java
@@ -1,13 +1,36 @@
 package com.cssrc.study.maven;
 
+import org.apache.log4j.PropertyConfigurator;
+
+import java.io.InputStream;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws Exception {
+        PropertyConfigurator.configure(App.class.getClassLoader().getResource("log4j.properties"));
+        int zipcode = 224400;
+        try {
+            zipcode = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+
+        }
+        new App(zipcode).start();
+    }
+
+    private int zip;
+
+    public App(int zip) {
+        this.zip = zip;
+    }
+
+    public void start() throws Exception {
+        // Retrieve Data
+        InputStream dataIn = new YahooRetriever().retrieve(zip);
+        // Parse Data
+        Weather weather = new YahooParser().parse(dataIn);
+        // Format (Print) Data
+        System.out.print(new WeatherFormatter().format(weather));
     }
 }
