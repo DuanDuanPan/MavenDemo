2f61c918ac6dd42ff3228c17f32ae7098cc9ddce
DuanDuan Pan <enjoyjavapan@163.com>
2017-05-03 14:50:23 +0800

    init project

---
+++ simple-weather/src/main/java/com/cssrc/study/maven/App.java
@@ -0,0 +1,13 @@
+package com.cssrc.study.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
